50d77c196b84345ae671c8e0dbb777e117bf4ab2
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-15 02:33:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,17 @@
 def unsafe?(speed)
-  if speed < 40
+  if speed < 40 || speed > 60
     true
-  else speed < 60
-    true
+  else speed > 40 || speed < 60
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-
+  if speed > 40
+    true
+  else speed < 60
+    true
+  end
 end
